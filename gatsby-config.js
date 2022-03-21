ef7d8ec487d3968d610d4c9e209f6693a11ad964
TeVeTed <o.tiapkin@gmail.com>
2022-03-21 23:03:59 +0200

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,19 @@
 module.exports = {
-  siteMetadata: {
-    title: `new`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+	siteMetadata: {
+		title: `new`,
+		siteUrl: `https://www.yourdomain.tld`
+	},
+	plugins: [
+		'gatsby-plugin-mdx',
+		'gatsby-plugin-image',
+		'gatsby-plugin-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'pages',
+				path: './src/pages/'
+			},
+			__key: 'pages'
+		}
+	]
 };
